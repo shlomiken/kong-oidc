51726129fcfb1613504f96253e4e6ce3bc4668b8
shlomiken <shlomi.cohen@kenshoo.com>
2018-05-22 11:22:24 +0300

    Support filter for the whole request uri, instead of only uri

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -3,7 +3,8 @@ local M = {}
 local function shouldIgnoreRequest(patterns)
   if (patterns) then
     for _, pattern in ipairs(patterns) do
-      local isMatching = not (string.find(ngx.var.uri, pattern) == nil)
+      local isMatching = not (string.find(ngx.var.request_uri, pattern) == nil)
+      ngx.log(ngx.DEBUG, "OidcHandler Matching " .. pattern .. " with " .. ngx.var.request_uri)
       if (isMatching) then return true end
     end
   end
